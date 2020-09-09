10760acb0ad5158f467dccf1474c55e4f780f0a2
Cole <cole@pop-os.localdomain>
2020-09-09 15:27:39 -0400

    final commit, game has working tests, completed logic, and updated styling in certain portions

--- app.py
+++ app.py
@@ -17,14 +17,15 @@ def home_page():
     return render_template("home.html")
 
 
-@app.route("/game", methods=["GET", "POST"])
+@app.route("/game", methods=["POST"])
 def game_main():
     """Main form where the game takes place?"""
     return render_template("game.html")
 
 
-@app.route("/check-word", methods=["GET", "POST"])
+@app.route("/check-word", methods=["GET"])
 def check_word():
+    """Check word route that takes the word submitted in the guess form and checks to see if it is a real word and\or on the board"""
     user_guess = request.args["word"]
     # print(user_guess)
     valid = boggle_game.check_valid_word(session["board"], user_guess)
@@ -34,14 +35,12 @@ def check_word():
 
 @app.route("/update-stats", methods=["POST"])
 def update_stats():
+    """this route is for updating the score stats including games played"""
     highscore = 0
     score = request.json["score"]
     if score > highscore:
         highscore = score
-    print(score)
-    print(highscore)
     session["highscore"] = highscore
     nplays = session.get("nplays", 0)
     session["nplays"] = nplays + 1
-    print(nplays)
     return jsonify({"score": score})
--- test.py
+++ test.py
@@ -5,6 +5,62 @@ from boggle import Boggle
 
 
 class FlaskTests(TestCase):
+    def setUp(self):
+        """Stuff to do before every test."""
+        self.client = app.test_client()
+        app.config["TESTING"] = True
 
-    # TODO -- write tests for every view function / feature!
+    # Boggle Tests
+    def test_read_dict(self):
+        """Checks to make sure the text dictionary can be opened"""
+        self.assertTrue(Boggle.read_dict(self, "words.txt"))
 
+    # Flask tests
+    def test_home(self):
+        """Checks that the home page renders all HTML attributes including session variables"""
+        with app.test_client() as client:
+            resp = client.get("/")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn(
+                '<h1 id="start" class="animated">The button below will start a game of Boggle!</h1>',
+                html,
+            )
+            self.assertIn(
+                '<button class="btn btn-mdb-color btn-lg btn-block">Start Boggle</button>',
+                html,
+            )
+            self.assertIn(
+                "<p><b>Games Played: &nbsp;</b> &nbsp;| &nbsp;<b>Highest Score: &nbsp;</b></p>",
+                html,
+            )
+            self.assertTrue(session["board"])
+            self.assertIsNone(session.get("highscore"))
+            self.assertIsNone(session.get("nplays"))
+
+    def test_check_valid_word(self):
+        """Checks the check word function in the Boggle class and its HTML response"""
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess["board"] = [
+                    ["C", "A", "T", "T", "T"],
+                    ["C", "A", "T", "T", "T"],
+                    ["C", "A", "T", "T", "T"],
+                    ["C", "A", "T", "T", "T"],
+                    ["C", "A", "T", "T", "T"],
+                ]
+        response = self.client.get("/check-word?word=cat")
+        self.assertEqual(response.json["result"], "ok")
+
+    def test_check_not_on_board_word(self):
+        """Checks the check word function to see if a word is valid, but not on the current board"""
+        self.client.get("/")
+        response = self.client.get("/check-word?word=inconceivable")
+        self.assertEqual(response.json["result"], "not-on-board")
+
+    def test_check_not_a_word(self):
+        """Checks the check word function to make sure it is picking up invalid words"""
+        self.client.get("/")
+        response = self.client.get("/check-word?word=dfasdfas")
+        self.assertEqual(response.json["result"], "not-word")
